77eb9ee43fcddb0966c61f6935c30b20940287dd
Anna-Mariia <122801694+bulo4ka032@users.noreply.github.com>
2023-05-20 17:20:26 +0300

    buttons auth

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,20 +1,42 @@
+import { useDispatch } from 'react-redux';
+import { loginUser, registerUser } from 'redux/auth/authOperations';
+
 const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget.elements;
+    const formValues = {
+      email: form.email.value,
+      password: form.password.value,
+    };
+    const submit = event.nativeEvent.submitter.name;
+    submit === 'login'
+      ? dispatch(loginUser(formValues))
+      : dispatch(registerUser(formValues));
+  };
+
   return (
     <div>
       <h1>You can log in with your Google Account:</h1>
       <a href="/">Google</a>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h2>Or log in using an email and password, after registering:</h2>
         <label>
           <p>Email</p>
-          <input type="email" placeholder="your@email.com" />
+          <input type="email" name="email" placeholder="your@email.com" />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" placeholder="••••••••" />
+          <input type="password" name="password" placeholder="••••••••" />
         </label>
-        <button type="submit">Log in</button>
-        <button type="submit">Registration</button>
+        <button name="login" type="submit">
+          Log in
+        </button>
+        <button name="register" type="submit">
+          Registration
+        </button>
       </form>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import AddTransaction from 'components/AddTransaction/AddTransatcion';
 import TransactionsList from 'components/TransactionsList/TransactionsList';
 import { Outlet } from 'react-router-dom';
+
 const Home = () => {
   <div>
     <Balance />
